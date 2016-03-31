be0e7d2012b2d560204a5bec3eec1ee3ecbcc840
Serkan Yersen <serkanyersen@gmail.com>
2016-03-31 15:25:24 -0700

    Thumbnails

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import path from 'path';
 import Hashids from 'hashids';
 import Locallydb from 'locallydb';
 import send from 'koa-send';
+import easyimg from 'easyimage';
 
 const routes = router();
 const hashids = new Hashids(':simple:image:upload');
@@ -32,17 +33,73 @@ routes.get('/', function* home() {
     });
 });
 
+routes.get('/thumb/:id.:ext', function* thumbGenerator(){
+    const images = this.db.collection('images');
+    const thumbs = this.db.collection('thumbs');
+    const id = hashids.decode(this.params.id);
+    const image = images.get(id[0]);
+
+    if (image) {
+        const src = `public/images/${image.name}`;
+        const path = `public/images/thumbs/${image.name}`;
+        let thumb = thumbs.where({ id: image.name }).items;
+
+        if (thumb.length === 0) {
+            if (image.mime === 'image/svg+xml') {
+                // just copy file
+                fs.createReadStream(src)
+                  .pipe(fs.createWriteStream(path));
+            } else if (image.mime === 'image/gif') {
+                yield easyimg.resize({
+                    src,
+                    dst: path,
+                    height: 100,
+                    width: 100,
+                    gravity: 'center',
+                    quality: 80
+                });
+            } else {
+                yield easyimg.thumbnail({
+                    src,
+                    dst: path,
+                    height: 100,
+                    width: 100,
+                    gravity: 'center',
+                    quality: 80
+                });
+            }
+
+            thumbs.insert({
+                src,
+                path,
+                id: image.name
+            });
+        }
+
+
+        yield send(this, path, {
+            maxage: 31536000
+        });
+
+    } else {
+        this.status = 404;
+        this.body = 'image not found';
+    }
+
+});
+
 routes.get('/:id.:ext', function* imageStatic() {
     const images = this.db.collection('images');
     const id = hashids.decode(this.params.id);
     const image = images.get(id[0]);
 
     if (image) {
+        const path = `public/images/${image.name}`;
         if (this.params.ext === 'json') {
-            this.body = image;
+            this.body = Object.assign({}, image, yield easyimg.info(path));
         } else {
             images.update(id[0], { views: (image.views || 0) + 1 });
-            yield send(this, `public/images/${image.name}`, {
+            yield send(this, path, {
                 maxage: 31536000
             });
         }
@@ -104,6 +161,7 @@ app.use(logger());
 
 app.use(function* setDB(next) {
     this.db = new Locallydb('./maindb');
+
     yield next;
 });
 
--- webpack/server.js
+++ webpack/server.js
@@ -41,6 +41,7 @@ module.exports = {
         'koa-send',
         'hashids',
         'locallydb',
+        'easyimage',
         'fs',
         'os',
         'path',
